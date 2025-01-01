43e52c29a21cead4116c0106771a3db4157835fe
Owen Simpson <owensimpson1127@gmail.com>
2025-01-01 13:48:18 -0500

    Fix: badges duplicated

--- src/app/(profile)/ProfileBadge.tsx
+++ src/app/(profile)/ProfileBadge.tsx
@@ -9,102 +9,24 @@ export function ProfileBadge(badge: {
     size: number
 }) {
     return (
-        <>
-            <TooltipContainer
-                $bottom
-                tooltipId={badge.id}
-                tooltipBody={
-                    <TooltipData $mt={0.5}>
-                        <p>
-                            <b>{badge.name}</b>
-                            <br />
-                            {badge.description}
-                        </p>
-                    </TooltipData>
-                }>
-                <CloudflareIcon
-                    path={badge.icon}
-                    alt={badge.name}
-                    width={badge.size}
-                    height={badge.size}
-                />
-            </TooltipContainer>
-            <TooltipContainer
-                $bottom
-                tooltipId={badge.id}
-                tooltipBody={
-                    <TooltipData $mt={0.5}>
-                        <p>
-                            <b>{badge.name}</b>
-                            <br />
-                            {badge.description}
-                        </p>
-                    </TooltipData>
-                }>
-                <CloudflareIcon
-                    path={badge.icon}
-                    alt={badge.name}
-                    width={badge.size}
-                    height={badge.size}
-                />
-            </TooltipContainer>
-            <TooltipContainer
-                $bottom
-                tooltipId={badge.id}
-                tooltipBody={
-                    <TooltipData $mt={0.5}>
-                        <p>
-                            <b>{badge.name}</b>
-                            <br />
-                            {badge.description}
-                        </p>
-                    </TooltipData>
-                }>
-                <CloudflareIcon
-                    path={badge.icon}
-                    alt={badge.name}
-                    width={badge.size}
-                    height={badge.size}
-                />
-            </TooltipContainer>
-            <TooltipContainer
-                $bottom
-                tooltipId={badge.id}
-                tooltipBody={
-                    <TooltipData $mt={0.5}>
-                        <p>
-                            <b>{badge.name}</b>
-                            <br />
-                            {badge.description}
-                        </p>
-                    </TooltipData>
-                }>
-                <CloudflareIcon
-                    path={badge.icon}
-                    alt={badge.name}
-                    width={badge.size}
-                    height={badge.size}
-                />
-            </TooltipContainer>
-            <TooltipContainer
-                $bottom
-                tooltipId={badge.id}
-                tooltipBody={
-                    <TooltipData $mt={0.5}>
-                        <p>
-                            <b>{badge.name}</b>
-                            <br />
-                            {badge.description}
-                        </p>
-                    </TooltipData>
-                }>
-                <CloudflareIcon
-                    path={badge.icon}
-                    alt={badge.name}
-                    width={badge.size}
-                    height={badge.size}
-                />
-            </TooltipContainer>
-        </>
+        <TooltipContainer
+            $bottom
+            tooltipId={badge.id}
+            tooltipBody={
+                <TooltipData $mt={0.5}>
+                    <p>
+                        <b>{badge.name}</b>
+                        <br />
+                        {badge.description}
+                    </p>
+                </TooltipData>
+            }>
+            <CloudflareIcon
+                path={badge.icon}
+                alt={badge.name}
+                width={badge.size}
+                height={badge.size}
+            />
+        </TooltipContainer>
     )
 }
